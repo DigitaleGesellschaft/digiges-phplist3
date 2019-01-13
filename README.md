0ece6e36393c70eb60ef91bded6b711229c5051a
Reto Schneider <code@reto-schneider.ch>
2019-01-13 21:14:58 +0100

    Add script for phpList update procedure

--- README.md
+++ README.md
@@ -1,26 +1,6 @@
 # Updating from Upstream
 
+Example:
 ```console
-cd /path/to/this/repository
-git checkout upstream-releases
-git rm -r phplist
-mkdir phplist
-tar xf ~/path/to/phplist/release/phplist-x.y.z.tgz --exclude='*.gitignore' --strip 1  -C phplist
-git add phplist
-git commit -m "phplist-x.y.z.tgz"
-git tag vx.y.z
-```
-
-# Merging Upstream to Master
-
-```console
-git checkout master
-git merge vx.y.z
-git tag vx.y.z-digiges
-```
-
-# Push to Repository
-
-```console
-git push origin upstream-releases master --tags
+./update.sh 3.3.8
 ```
---
+++ update.sh
@@ -0,0 +1,19 @@
+# Usage: update.sh <version>
+
+set -eu -o pipefail
+
+version=$1
+
+git checkout upstream-releases
+git rm -r phplist
+mkdir phplist
+wget "https://sourceforge.net/projects/phplist/files/phplist/${version}/phplist-${version}.tgz/download" -O phplist-${version}.tgz
+tar xf phplist-${version}.tgz --exclude='*.gitignore' --strip 1  -C phplist
+rm phplist-${version}.tgz
+git add phplist
+git commit -m "phplist-${version}.tgz"
+git tag v${version}
+git checkout master
+git merge v${version}
+git tag v${version}-digiges
+git push origin upstream-releases master --tags
